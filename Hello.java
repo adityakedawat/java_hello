332338675435da7e33911266a70608154c836b45
Aditya Khandelwal <adityakedawat@gmail.com>
2021-07-06 14:03:28 +0530

    second commit

--- Hello.java
+++ Hello.java
@@ -1,11 +1,7 @@
-
-
-public class Hello {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
-
-	}
-
+class Hello {
+   public static void main(String[] args){
+      System.out.println("Hello World");
+	  System.out.println("Hello Jenkins");
+	  System.out.println("Hello World");
+   }
 }
